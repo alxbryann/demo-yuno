d550852a669dc5cbf60d6a7e08354a3dd77a8c2f
Mr uprizing <uprizingfaze@gmail.com>
2025-12-14 09:51:18 -0500

    Fix: Ui sdk

---
+++ apps/website/src/app/frame/page.tsx
@@ -0,0 +1,9 @@
+import YunoIframe from "yuno-demo-sdk-ui";
+
+export default function Page() {
+  return (
+    <div>
+      {/*<YunoIframe url="http://localhost:3000/test" height="800px" />*/}
+    </div>
+  );
+}
